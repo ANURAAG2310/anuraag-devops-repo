8b66b0a215dbc537ae4b5ed1b9d7bfef14e2b111
ANURAAG2310 <ratakondaanuraag@gmail.com>
2024-09-09 18:49:08 +0530

    feature2.2 changes added by vivek in nov2023

--- application.java
+++ application.java
@@ -24,7 +24,31 @@ public class GFG {
 
 		return result;
 	}
-
+#feature 2.2 chages done by vivek in nov 2023
+class ComplexNumber {
+ 
+    // variables to hold real and imaginary part of complex
+    // number
+    int real, image;
+ 
+    // Constructor which will be used while creating complex
+    // number
+    public ComplexNumber(int r, int i)
+    {
+        this.real = r;
+        this.image = i;
+    }
+ 
+    // function to print real number
+    public void showC()
+    {
+        System.out.println(this.real + " +i " + this.image);
+    }
+ 
+    // we will implement  this function for addition
+    public complex add(ComplexNumber, ComplexNumber);
+}
+#end of feature 2.2 changes done by vivek in 2023
 	// Main driver method
 	public static void main(String args[])
 	{
